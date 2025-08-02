3c8d3c60c6c30af38fb0badcd00fd5226dd36dde
kaif-saifi <kaifsaifi864@gmail.com>
2025-08-02 09:24:33 +0530

    Update: fixed MongoDB connection and added .env support

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const port = 2000
 const app = express();
+require('dotenv').config();
 
 
 const session = require('express-session');
@@ -39,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     useUnifiedTopology: true,
 // })
 app.use(bodyParser.urlencoded({ extended: false }))
-mongoose.connect(process.env.MONGO_URI2, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
