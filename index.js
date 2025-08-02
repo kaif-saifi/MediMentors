1a5484b071933d6646c07d310318604b87c3e6a0
kaif-saifi <kaifsaifi864@gmail.com>
2025-08-02 22:25:22 +0530

    Changed nodemon to node for Render deploy

--- index.js
+++ index.js
@@ -40,10 +40,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     useUnifiedTopology: true,
 // })
 app.use(bodyParser.urlencoded({ extended: false }))
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection
 db.once('open', () => {
     console.log("connected to mongoose")
